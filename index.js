6f9d73f0374004965f0a7594bff6235b63f443c7
hdekjsne <hdekjsne@gmail.com>
2023-04-30 08:06:38 +0300

    add some useful stuff

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const content = fs.readFileSync(path.join(
 ), 'utf-8');
 
 // BEGIN
+const i = {
+  name: 0,
+  force: 1,
+  hp: 2,
+  amount: 3,
+  height: 4,
+  weight: 5,
+  price: 6,
+};
+
 const processedData = content
   .split('\r\n')
   .map((line) => line
@@ -27,6 +37,9 @@ const processedData = content
 
 const creatureTypes = processedData.reduce((acc) => acc + 1, 0);
 
-const result = `Total amount of creature types: ${creatureTypes}`;
+const result = `Total amount of creature types: ${creatureTypes}\n
+To hire 10 most powerful creatures () would cost you: \n
+To hire 20 second powerful creatures () would cost you: `;
+
 console.log(result);
 // END
